5c1ca8ef0b67afd62c94fc06a581483f4753f960
Denis Lazo <denislazo1610@gmail.com>
2022-04-26 01:47:00 -0600

    learning more about express

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("HJordi Castro");
-});
+// app.get("/", (req, res) => {
+//   res.send("HJordi Castro");
+// });
 
-app.listen(port);
+app.use("/", require("./routes"));
+
+app.listen(port, () => {
+  console.log("this is running");
+});

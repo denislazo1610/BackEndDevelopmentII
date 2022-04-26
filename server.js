28055d6534754a6899562c7d7cec1e18876c44ae
Denis Lazo <denislazo1610@gmail.com>
2022-04-26 01:13:16 -0600

    fixing

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("HJordi Castro");
 });
 
-app.listen(3000);
+app.listen(port);

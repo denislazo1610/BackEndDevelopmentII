d351febb4058d1f93e86356d70d7056eac2b12b5
Denis Lazo <denislazo1610@gmail.com>
2022-05-26 01:44:05 -0600

    Team hw completed

--- controller/contacts.js
+++ controller/contacts.js
@@ -3,11 +3,21 @@ const contact = require('../DB/schemaContact');
 const Joi = require('@hapi/joi');
 var createError = require('http-errors');
 const { schemaContactValidation } = require('../DB/schemaValidation');
+const { default: mongoose } = require('mongoose');
 
-const gettingInfoContacts = (req, res) => {
-  contact.find().then((result) => {
-    res.send(result);
-  });
+const gettingInfoContacts = (req, res, next) => {
+  contact
+    .find()
+    .then((result) => {
+      if (result.length == 0) {
+        throw createError(405, 'There is nothing');
+      } else {
+        res.send(result);
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
 };
 
 const gettingSingleContact = (req, res, next) => {
@@ -15,7 +25,6 @@ const gettingSingleContact = (req, res, next) => {
   contact
     .findById(id)
     .then((result) => {
-      // !result ? next(createError(405, 'Not Contact found')) : res.send(result);
       if (!result) {
         throw createError(405, 'Not contact found');
       } else {
@@ -28,13 +37,24 @@ const gettingSingleContact = (req, res, next) => {
     });
 };
 
-const deletingSingleContact = (req, res) => {
-  contact.findByIdAndDelete(req.params.id, function () {
-    console.log('deleted');
-    contact.find().then((result) => {
-      res.send(result);
+const deletingSingleContact = (req, res, next) => {
+  const id = req.params.id;
+  contact
+    .findById(id)
+    .then((result) => {
+      if (!result) {
+        throw createError(405, 'This contact did not exist');
+      } else {
+        contact.findByIdAndDelete(id, function () {
+          contact.find().then((result) => {
+            if (result) res.send(result);
+          });
+        });
+      }
+    })
+    .catch((error) => {
+      next(error);
     });
-  });
 };
 
 const creatingNewContact = (req, res, next) => {
@@ -42,23 +62,27 @@ const creatingNewContact = (req, res, next) => {
 
   try {
     const { value, error } = schemaContactValidation.validate(newContacto);
-
     // If error exists from validation, then it will create an error with a message and its status
     if (error) throw createError(405, 'Input invalid, check if you email is correct');
     contact.insertMany(newContacto);
     res.send(newContacto);
   } catch (error) {
-    res.status(500).json({ message: error });
     next(error);
   }
 };
 
-const updatingContact = (req, res) => {
+const updatingContact = (req, res, next) => {
   var updatedContact = req.body;
 
-  contact.findByIdAndUpdate(req.params.id, updatedContact, function () {
-    console.log('Updated!');
-  });
+  try {
+    const { value, error } = schemaContactValidation.validate(updatedContact);
+    if (error) throw createError(405, 'Input invalid, check if you email is correct');
+    contact.findByIdAndUpdate(req.params.id, updatedContact, function () {
+      res.send(updatedContact);
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
